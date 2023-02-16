87d884af7a5f137c34ec4d5a3f92bd03c3c3d30b
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-02-15 20:21:57 -0500

    finished with html css and js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 let match = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/
+const input = document.getElementById("input");
+const buttion = document.querySelector(".buttion")
+const hide = document.querySelector(".hide")
+const email = document.querySelector(".email")
+function error () {
+     if(input.value.match(match) == null) {
+        email.classList.add("red");
+        hide.classList.remove("hide")
+        hide.classList.add("show")
+        input.value = null
+     }else {
+        email.classList.remove("red");
+        hide.classList.add("hide")
+        hide.classList.remove("show")
+     }
+    
+}
+
+
+
+buttion.addEventListener("click", error)
